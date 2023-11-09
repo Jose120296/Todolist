151dedfca365eb8b6e5ee81183caeefa8b2d9682
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2023-11-09 00:40:58 +0000

    Final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,55 +1,51 @@
-import React, {useState} from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-//create your first component
 const Home = () => {
-	const[inputValue, setInputValue] = useState("");
-	const[todos, setTodos ]= useState ([]);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-	//Add into array => concat
-	//Delate from array => filter 
-	//Update => map
+  return (
+    <div className="container">
+      <h1>My List</h1>
+      <ul>
+        <li>
+          <input
+            id="myInput"
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="Asigna una tarea."
+          />
+        </li>
 
-	return (
-		<div className="container">
-			<h1>My List</h1>
-			<ul>
-				<li>
-					<input 
-					id="myInput"
-					type="text" 
-					onChange={(e)=> setInputValue(e.target.value)} 
-					value={inputValue}
-					onKeyDown={(e) => { 
-						if (e.key=== "Enter") {
-							setTodos(todos.concat(inputValue)); 
-							setInputValue("") 
-						}
-					}}
-					placeholder="Asigna una tarea."></input>
-				</li>
-				{todos.map ((item, index) => (
-                    <li key={index}>
-                    {item}{" "}
-                    <FontAwesomeIcon 
-                      icon={faTrash}
-                      className="animate-icon hidden-icon"
-                      onClick={() =>
-                        setTodos(
-                            todos.filter(
-                                (t, currentIndex) => 
-                                index !== currentIndex))
-                      }
-                    />
-                    </li>
-				))}
-			</ul>
-			<div>{todos.length} tasks</div>
-			<div>No hay tareas, agrega una.</div>
-		</div>
-	);
+        {todos.length === 0 ? (
+          <div className="task-counter">No tasks, add a task!</div>
+        ) : (
+          todos.map((item, index) => (
+            <li key={index}>
+              {item}{" "}
+              <FontAwesomeIcon
+                icon={faTrash}
+                className="animate-icon hidden-icon"
+                onClick={() =>
+                  setTodos(todos.filter((t, currentIndex) => index !== currentIndex))
+                }
+              />
+            </li>
+          ))
+        )}
+      </ul>
+      <div>{todos.length} tasks</div>
+    </div>
+  );
 };
-
 
 export default Home;
