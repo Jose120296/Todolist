f96ea0580eccda96dc962c7ff97c96176fff8d7b
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2023-11-17 01:08:06 +0000

    boton eliminar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,49 +6,57 @@ const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
   const [apiData, SetapiData] = useState([]);
-  
+  const [showButton, setShowButton] = useState(false);
 
   return (
-    <div className="container">
-      <h1>My List</h1>
-      <ul>
-        <li>
-          <input
-            id="myInput"
-            type="text"
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                setTodos(todos.concat(inputValue));
-                setInputValue("");
-              }
-            }}
-            placeholder="Asigna una tarea."
-          />
-        </li>
-
-        {todos.length === 0 ? (
-          <div className="task-counter">No tasks, add a task!</div>
-        ) : (
-          todos.map((item, index) => (
-            <li key={index}>
-              {item}{" "}
-              <FontAwesomeIcon
-                icon={faTrash}
-                className="animate-icon hidden-icon"
-                onClick={() =>
-                  setTodos(todos.filter((t, currentIndex) => index !== currentIndex))
-                }
-              />
-            </li>
-          ))
-        )}
-      </ul>
-      <div>{todos.length} tasks</div>
-      
-    </div>
-  );
-};
-
-export default Home;
+		<div className="container">
+		  <h1>My List</h1>
+		  <ul>
+			<li>
+			  <input
+				id="myInput"
+				type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyDown={(e) => {
+				  if (e.key === "Enter") {
+					setTodos(todos.concat(inputValue));
+					setInputValue("");
+					setShowButton(true);
+				  }
+				}}
+				placeholder="Asigna una tarea."
+			  />
+			</li>
+	
+			{todos.length === 0 ? (
+			  <div className="task-counter">No tasks, add a task!</div>
+			) : (
+			  todos.map((item, index) => (
+				<li key={index}>
+				  {item}{" "}
+				  <FontAwesomeIcon
+					icon={faTrash}
+					className="animate-icon hidden-icon"
+					onClick={() =>
+					  setTodos(todos.filter((t, currentIndex) => index !== currentIndex))
+					}
+				  />
+				</li>
+			  ))
+			)}
+		  </ul>
+		  <div>{todos.length} tasks</div>
+		 {showButton && (
+		   <button
+			 className="delete-all-button"
+			 onClick={() => setTodos([])}
+		   >
+			 Eliminar todas las tareas
+		   </button>
+		 )}
+		</div>
+	  );
+	};
+	
+	export default Home;
