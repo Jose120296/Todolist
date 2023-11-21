c2367d9ae6735fb2b0825e0e47014f76171eb02e
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2023-11-21 01:48:29 +0000

    revision

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,8 +5,8 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
-  const [apiData, SetapiData] = useState([]);
-  const [setShowButton] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 		
 const crearUsuario = async () => {
 		try {
@@ -26,7 +26,43 @@ const crearUsuario = async () => {
 		} catch (error) {
 			console.error("Error al crear el usuario:", error);
 		}
-	}
+	};
+
+	const handleDeleteTasks = async () => {
+		try{
+			setIsDeleting(true);
+
+			const updatedTodos = todos.filter((t, currentIndex) => index !== currentIndex
+			);
+			console.log("updatedTodos", updatedTodos);
+			setTodos(updatedTodos);
+			await syncWithApi(updatedTodos);
+			await getTodos();
+		} catch (error) {
+			console.log("error en el handleDeleteTasks", error);
+		} finally {
+			setIsDeleting(false);
+			
+		}
+	};		
+
+	const handleDeleteTasks2 = async (index) => {
+		try{
+			setIsDeleting(true);
+			const updatedTodos = todos.filter((t, currentIndex) => index !== currentIndex
+			);
+			console.log("updatedTodos", updatedTodos);
+			setTodos(updatedTodos);
+			await syncWithApi(updatedTodos);
+			await getTodos();
+		} catch (error) {
+			console.log("error en el handleDeleteTasks", error);
+		} finally {
+			setIsDeleting(false);
+			
+		}
+	};
+	
 
 	const handleDelete = async () => {
 	  try {
@@ -46,31 +82,95 @@ const crearUsuario = async () => {
 	  } catch (error) {
 		console.error("Error al crear el usuario:", error);
 	  }
-	}
+	};
 
-	
+	const getTodos = async () => {
+	  try {
+		const response = await fetch(
+		  "https://playground.4geeks.com/apis/fake/todos/JoseEnrique"
+		);
+		
+		if (response.status === 404) {
+			console.log("No se encontraron datos");
+			await crearUsuario();
+			return;
+		}
 
+		const data = await response.json();
+		console.log("Fetched data:", data);
+
+		setTodos(data || []);
+		} catch (error) {
+		
+		console.error("Error fetching data:", error);
+		}
+	};
+
+	const syncWithApi = async (updatedtodos) => {
+
+		try {
+			const response = await fetch("https://playground.4geeks.com/apis/fake/todos/JoseEnrique",
+			{
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(updatedtodos),
+			}
+		);
+
+		if (response.status !== 200) {
+			return false;
+		} 
+
+			const data = await response.json();
+			console.log("Sync with API response", data);
+			setErrorMessage("");
+			return true;
+		} catch (error) {
+			console.error("Error syncing with API:", error);
+			setErrorMessage("Error syncing with API");
+		}
+	};
+
+	const handleInputChange = (e) => {
+		setInputValue(e,target.value);
+	};
+
+	const handleEnterKey = async (e) => {
+		if (e.key === "Enter" && inputValue.trim() !== "") {
+			const newTodos = {label: inputValue.trim(), done: false};
+
+			if (task.some((t) => t.label === newTodos.label)) {
+				setErrorMessage("Task already exists");
+				return;
+			}
+
+			const updatedTodos = [...todos, newTodos];
+			setInputValue("");
+			const updated = await syncWithApi(updatedTodos);
+			if (updated) {
+				await getTodos();
+			}
+		}	
+	};
 
   	return (
 	<div className="container">
-	  <h1>My List</h1>
+		<div>	
+	  	<h1>My List</h1>
+		</div>  
 	  <ul>
-		<li>
+		<div>
 		  <input
 			id="myInput"
 			type="text"
-			onChange={(e) => setInputValue(e.target.value)}
+			onChange={handleInputChange}
 			value={inputValue}
-			onKeyDown={(e) => {
-			  if (e.key === "Enter") {
-				setTodos(todos.concat(inputValue));
-				setInputValue("");
-				setShowButton(true);
-			  }
-			}}
+			onKeyDown={handleEnterKey}
 			placeholder="Asigna una tarea."
 		  />
-		</li>
+		</div>
   
 		{todos.length === 0 ? (
 		  <div className="task-counter">No tasks, add a task!</div>
@@ -81,9 +181,7 @@ const crearUsuario = async () => {
 			  <FontAwesomeIcon
 				icon={faTrash}
 				className="animate-icon hidden-icon"
-				onClick={() =>
-				  setTodos(todos.filter((t, currentIndex) => index !== currentIndex))
-				}
+				onClick={() => handleDeleteTasks}
 			  />
 			</li>
 		  ))
